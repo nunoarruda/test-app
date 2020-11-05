9db6473620f0ae8999174c6858090220d67cf950
Nuno Arruda <nuno@nunoarruda.com>
2020-11-05 14:38:30 -0100

    Create Express server for Heroku

---
+++ express-server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('./dist/test-app'));
+
+app.get('/*', (req, res) => res.sendFile('index.html', {root: 'dist/test-app/'}));
+
+app.listen(process.env.PORT || 8080);
